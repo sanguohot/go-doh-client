a2c5f77687dc09be71d4b766a4d62591443d4da0
evan <281406674@qq.com>
2020-02-29 16:56:39 +0800

    3s超时修改为2s超时；支持通过环境变量关闭证书校验

--- http.go
+++ http.go
@@ -2,9 +2,11 @@ package doh
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -25,8 +27,16 @@ func exchangeHTTPS(q []byte, resolver string) (a []byte, err error) {
 	req.Header.Add("Content-Type", "application/dns-message")
 
 	client := http.Client{
-		Timeout: 3 * time.Second,
+		Timeout: 2 * time.Second,
 	}
+	if os.Getenv("DOH_VERIFY") == "off" {
+		fmt.Println("doh do not verify certificate as DOH_VERIFY=off")
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client.Transport = tr
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return
